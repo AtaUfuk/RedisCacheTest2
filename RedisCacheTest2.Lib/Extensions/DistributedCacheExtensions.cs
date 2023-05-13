7605283aa0b64fe7e72644d266c53303e35d484e
Ufuk Ata <45665853+AtaUfuk@users.noreply.github.com>
2023-05-13 17:56:38 +0300

    Add project files.

---
+++ RedisCacheTest2.Lib/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System.Text;
+using System.Text.Json;
+
+namespace RedisCacheTest2.Lib.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetorUpdateRecordAsync<T>(this IDistributedCache cache, string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+            if (IsExistRecord(cache, recordId))
+            {
+                cache.Remove(recordId);
+            }
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T?> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+            if (object.Equals(jsonData, null))
+            {
+                return default;
+            }
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+
+        public static bool IsExistRecord(IDistributedCache cache, string recordId)
+        {
+            var jsonData = cache.GetString(recordId);
+            bool result = false;
+            if (!object.Equals(jsonData, null))
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static void ClearAll(this IDistributedCache cache)
+        {
+
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", false);
+            IConfiguration configuration = builder.Build();
+            var connection = configuration.GetConnectionString("Redis");
+            var _connectionMultiplexer = ConnectionMultiplexer.Connect(connection+ ",allowAdmin=true");
+            var endpoint = _connectionMultiplexer.GetEndPoints(true).First();
+            var server = _connectionMultiplexer.GetServer(endpoint);
+            server.FlushAllDatabases();
+        }
+    }
+}
---
+++ RedisCacheTest2.RestApi/Controllers/RedisCacheController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using RedisCacheTest2.Lib.Extensions;
+
+namespace RedisCacheTest2.RestApi.Controllers
+{
+    [Route("/redis-cache")]
+    [ApiController]
+    public class RedisCacheController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public RedisCacheController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        [Route("/index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Index Called");
+        }
+        [Route("/clear-all")]
+        [HttpGet]
+        public IActionResult ClearCache()
+        {
+            _distributedCache.ClearAll();
+            return Ok("Clear Cache Called");
+        }
+    }
+}
